6c697bf706b881e9350e52316ad88abd5753c7fa
ysjysj1212 <dbstjdwns1234@gmail.com>
2026-01-12 17:43:16 +0900

    refactor: 엔드포인트 prefix "api" 중복 제거

--- src/main/java/com/meta/springmvc/controller/HelloController.java
+++ src/main/java/com/meta/springmvc/controller/HelloController.java
@@ -4,32 +4,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("api")
 public class HelloController {
-    @GetMapping("api/hello")
+    @GetMapping("hello")
     @ResponseBody
     public String hello() {
         return "Hello world!";
     }
 
-    @GetMapping("api/get")
+    @GetMapping("get")
     @ResponseBody
     public String get(){
         return "GET Method 요청";
     }
 
-    @PostMapping("api/post")
+    @PostMapping("post")
     @ResponseBody
     public String post() {
         return "POST Method 요청";
     }
 
-    @PutMapping("api/put")
+    @PutMapping("put")
     @ResponseBody
     public String put() {
         return "PUT Method 요청";
     }
 
-    @DeleteMapping("api/delete")
+    @DeleteMapping("delete")
     @ResponseBody
     public String delete() {
         return "DELETE Method 요청";
