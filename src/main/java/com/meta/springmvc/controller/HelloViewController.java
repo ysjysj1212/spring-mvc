2e06c3f5c3e0c6fd110e8ccc5d6b45d7330e1236
ysjysj1212 <dbstjdwns1234@gmail.com>
2026-01-13 12:27:17 +0900

    feat: Model 객체를 통한 동적데이터바인딩

--- src/main/java/com/meta/springmvc/controller/HelloViewController.java
+++ src/main/java/com/meta/springmvc/controller/HelloViewController.java
@@ -1,10 +1,13 @@
 package com.meta.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloViewController {
+    private static long visitCount = 0;
+
     @GetMapping("/static-hello")
     public String hello() {
         return "hello.html";
@@ -21,5 +24,12 @@ public class HelloViewController {
         return "hello-template";
         // prefix로 컨텍스트 경로가 붙는다.
         // surfix로 .html 처럼 정적 페이지 확장자가 자동으로 붙는다.
+    }
+
+    @GetMapping("html/dynamic")
+    public String htmlDynamic(Model model) {
+        visitCount++;
+        model.addAttribute("visits", visitCount);
+        return "hello-dynamic";
     }
 }
