e5b221af6ab36ba4edc1b38944e293a7fae41aaf
ysjysj1212 <dbstjdwns1234@gmail.com>
2026-01-14 16:28:42 +0900

    feat: PathVariable 방식 요청 받는 컨트롤러 작성법

--- src/main/java/com/meta/springmvc/controller/DataRequestController.java
+++ src/main/java/com/meta/springmvc/controller/DataRequestController.java
@@ -2,7 +2,9 @@ package com.meta.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("hello/request")
@@ -10,5 +12,12 @@ public class DataRequestController {
     @GetMapping("form/html")
     public String form() {
         return "hello-request-form";
+    }
+
+
+    @GetMapping("/star/{name}/age/{age}")
+    @ResponseBody
+    public String helloRequestPath(@PathVariable String name, @PathVariable int age) {
+        return String.format("Hello, @PathVariable로 받은 데이터값들, <br> name = %s age = %d", name, age);
     }
 }
