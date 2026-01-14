f90273e9bcdfb0cbad78ef30631555476570ae42
ysjysj1212 <dbstjdwns1234@gmail.com>
2026-01-14 16:35:51 +0900

    feat: QueryString 방식 요청 받는 컨트롤러 작성법

--- src/main/java/com/meta/springmvc/controller/DataRequestController.java
+++ src/main/java/com/meta/springmvc/controller/DataRequestController.java
@@ -1,10 +1,7 @@
 package com.meta.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("hello/request")
@@ -19,5 +16,12 @@ public class DataRequestController {
     @ResponseBody
     public String helloRequestPath(@PathVariable String name, @PathVariable int age) {
         return String.format("Hello, @PathVariable로 받은 데이터값들, <br> name = %s age = %d", name, age);
+    }
+
+    @GetMapping("form/param")
+    @ResponseBody
+    public String helloRequestParam(@RequestParam String name, @RequestParam int age) {
+        return String.format("Hello, @RequestParam으로 받은 값들,<br> name = %s age = %d", name, age );
+
     }
 }
